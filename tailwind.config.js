80e49d4345521de506cdcb4e0f23b5506f477614
xh14235 <15021161540@163.com>
2024-03-26 21:50:55 +0800

    fix: 解决tailwindcss样式覆盖问题

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,14 +3,5 @@ module.exports = {
   theme: {
     extend: {},
   },
-  plugins: [
-    // eslint-disable-next-line func-names
-    ({ addBase }) => {
-      addBase({
-        "button, [type='button'], [type='reset'], [type='submit']": {
-          "background-color": "null",
-        },
-      });
-    },
-  ],
+  plugins: [],
 };
